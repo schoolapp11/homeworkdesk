9fefaa577d8673a8bfc1decabcf85bfde600935a
schoolapp11 <165368807+schoolapp11@users.noreply.github.com>
2024-04-07 09:22:29 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+nameOfCustomer = input("HI!! welcome to our store. /n whats your name?")
+
+if nameOfCustomer == "ben" or nameOfCustomer == "patricia":
+  print("ahhhhh fuck off " + nameOfCustomer)
+  exit()
+elif nameOfCustomer == int:
+  print("this > " + nameOfCustomer + "is not a valid character type")
+  input("Please input your name \n")
+
+
